ea44bb74c0629152a63eaf0e5ecf003103f1e930
hongloans9 <nguyenloan150492@gmail.com>
2017-10-08 02:00:48 +0300

    get weather API - Axios AJAX request

---
+++ src/actions/index.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+
+const API_KEY = "e18b7bede7d6ffcd4a2445765fcf8bee";
+const ROOT_URL = "http://api.openweathermap.org/data/2.5/forecast?appid=" + API_KEY;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city){
+
+    const url = ROOT_URL + "&q=" + city + ",fi";
+    const request = axios.get(url);
+
+    return{
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
